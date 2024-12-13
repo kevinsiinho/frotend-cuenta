d355fec89a7087b26a7d0ba7de9cbbf870e4230e
kevinsiinho <kevinsiinho@gmail.com>
2024-12-13 17:25:50 -0500

    socket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { environment } from 'src/environments/environment.prod';
 
-const config: SocketIoConfig = { url: environment.url, options: {} }; // Cambia la URL a tu servidor
 
 
 @NgModule({
@@ -20,7 +15,6 @@ const config: SocketIoConfig = { url: environment.url, options: {} }; // Cambia 
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
-    SocketIoModule.forRoot(config)
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
